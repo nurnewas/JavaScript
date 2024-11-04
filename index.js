eb9825490a1f4d4a460c6d47493aed06337f2b46
nurnewas <nurnewasjr@gmail.com>
2024-11-03 23:19:40 -0600

    parsetFloat, parsetInt and toFixed

--- index.js
+++ index.js
@@ -1,7 +1,27 @@
-console.log("Starting");
+// console.log("Starting");
 var name =" Nur Newas";
-console.log(name);
+// console.log(name);
 
-
+const greeting = true;
 const age = 19 ;
-console.log(age);
+// console.log(age);
+
+// console.log(typeof greeting );
+
+const person = 20;
+const person2 = parseInt("10");
+
+
+// console.log(person +  person2);
+
+const person3 = 20;
+const person4 = parseFloat(22.4);
+
+// console.log(person3 + person4);
+const a = 0.1 ;
+const b = 0.2 ;
+const total = a + b;
+console.log(total.toFixed(1));
+// console.log(total.toFixed(2));
+
+
