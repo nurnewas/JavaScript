a7b734e7c95d469cbc0ea8a210b28f3088116a9c
nurnewas <nurnewasjr@gmail.com>
2024-11-08 03:45:07 -0600

    index

--- array.js
+++ array.js
@@ -2,11 +2,16 @@ const num = [12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23];
 const name = ["nur1", "nur2", "nur3", "nur4", "nur5"];
 const mix =  [12, "nur", true, false, 1];
 
-console.log(num +" " + name);
-console.log(name.length +"&" +num.length );
-console.log(name.length +" " + num.length +" " +  mix.length);
+// console.log(num +" " + name);
+// console.log(name.length +"&" +num.length );
+// console.log(name.length +" " + num.length +" " +  mix.length);
 
-mix[3] = true;
+// mix[3] = true;
 
-console.log(mix);
-// console.log(`${num} and ${name}`);
+// console.log(mix);
+// // console.log(`${num} and ${name}`);
+
+console.log(name[3]);
+name.push(mix[3]);
+
+console.log(num +" " + name+ " " + mix);
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,1 +1,7 @@
-Nur
+console.log(name.length +"&" +num.length );
+// console.log(name.length +" " + num.length +" " +  mix.length);
+
+// mix[3] = true;
+
+// console.log(mix);
+// // console.log(`${num} and ${name}`);
