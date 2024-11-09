aacfa2c65c3874695993650bb618864e68290777
nurnewas <nurnewasjr@gmail.com>
2024-11-09 01:12:53 -0600

    while Loop

---
+++ range.js
@@ -0,0 +1,9 @@
+let num = 0 ;
+let sum = 0 ;
+while (num <=3){
+    // console.log(num);
+    sum += num;
+    console.log(sum);
+    num++;
+}
+
---
+++ whileloop.js
@@ -0,0 +1,7 @@
+let num = 0;
+
+while (num < 5) {
+  console.log("looping" + num);
+//   num++; //(number asba then add hoba)
+  // ++num; //(number add then add number asaba)
+}
