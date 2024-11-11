dc7b2f236365abdfc84503141e350091b2529221
nurnewas <nurnewasjr@gmail.com>
2024-11-11 00:07:01 -0600

    Split & JOin

--- slice.js
+++ slice.js
@@ -1,6 +1,14 @@
-const name = 'Md. Nur Newas Jr';
+const name = 'Md,Nur,Newas,Jr';
 const part =name.slice(3, 7);
-console.log(part);
+// console.log(part);
 
 const part2 =name.slice(0, name.length);
-console.log(part2);
+// console.log(part2);
+
+
+const short = name.split(' ');
+// console.log(short);
+const n = name.split(',')
+
+console.log(n.join());
+console.log(n.join("-"));
