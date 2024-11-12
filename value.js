5a82968b6e4d78b2e47faddfe3b032c7a7bed1db
nurnewas <nurnewasjr@gmail.com>
2024-11-11 22:02:05 -0600

    mChange Value

---
+++ value.js
@@ -0,0 +1,14 @@
+const person = {
+    name: "sodor Uddin",
+    age: 19,
+    pro:" Developer",
+    sallary: 30000,
+    married: true,
+    'Favorites placement': ['bandar bon', 'pana bon']
+}
+
+person.sallary = 35000;
+console.log(person.sallary);
+
+person.age = 25;
+console.log(person.age);
