e10e20f16fd3b926f0ff03e56e88f3855752827e
nurnewas <nurnewasjr@gmail.com>
2024-11-11 23:41:26 -0600

    Nested Object

---
+++ nestaed.js
@@ -0,0 +1,13 @@
+const collage = {
+    name: "College of Arts and Sciences",
+    departments: ["Arts", "Science", "Commerce", "Humanities"],
+    faculty: ["Faculty of Arts", "Faculty of Science", "Faculty of Commerce", "Faculty of Humanities"],
+    teacher: {
+        name: "Dr. Nur Newas",
+        age: 50,
+        department: "Faculty of Arts"
+    }
+}
+console.log(collage);
+console.log(collage.teacher);
+console.log(collage.teacher.name);
