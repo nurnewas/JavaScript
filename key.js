7112adbd07234c843672c1acfc594b374b9ddb1c
nurnewas <nurnewasjr@gmail.com>
2024-11-11 23:34:42 -0600

    get Key and Vlaues

---
+++ key.js
@@ -0,0 +1,13 @@
+const computer = {
+    name: 'Dell',
+    manufacturer: 'Dell Inc.',
+    operatingSystem: 'Windows 10',
+    processor: 'Intel Core i7',
+    memory: '8GB',
+    graphics: 'AMD Graphics',
+};
+const keys = Object.keys(computer);
+console.log(keys);
+
+const values = Object.values(computer);
+console.log(values);
